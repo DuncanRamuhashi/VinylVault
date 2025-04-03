81951649624119e11be5538b4718799abb04b2dd
Duncan Ramuhashi <duncanramohashi@gmail.com>
2025-04-03 12:15:27 +0200

    sdfsd

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,18 @@
 import express from 'express';
-
+import cookieParser from "cookie-parser";
+import { Env_Consts } from './constants/envConsts';
+import { STATUS_CODES } from './constants/httpCodes';
 const app =  express();
 
-app.listen(5000 ,() =>{
-    console.log("App is running");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/',(req,res) =>{
+    res.status(STATUS_CODES.OK).json("Welcome to the V-Api");
+});
+
+app.listen(Env_Consts.PORT ,async () =>{
+    console.log(`connected at PORT : ${Env_Consts.PORT}`);
+
 });
---
+++ backend/src/constants/envConsts.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+interface EnvConsts {
+    PORT: string | number;
+    CONNECTION_STRING: string | undefined;
+    NODE_ENV: string | undefined;
+    JWT_SECRET: string | undefined;
+}
+
+export const Env_Consts: EnvConsts = {
+    PORT: process.env.PORT || 5001,
+    CONNECTION_STRING: process.env.CONNECTION_STRING,
+    NODE_ENV: process.env.NODE_ENV,
+    JWT_SECRET: process.env.JWT_SECRET
+};
