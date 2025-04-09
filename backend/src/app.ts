cfb78ea71af82103eb0752109b66d4f24800e175
Duncan Ramuhashi <duncanramohashi@gmail.com>
2025-04-09 16:32:47 +0200

    ddd

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,28 +10,24 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(express.json({ limit: '5mb' })); // Added limit here
-app.use(express.urlencoded({ extended: true, limit: '5mb' })); // Added limit here
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' })); 
 app.use(cookieParser());
 app.use(cors({
     origin: Env_Consts.FRONTEND_URL,
     credentials: true
 }));
 
-// Health check endpoint
 app.get('/', (req, res) => {
     res.status(STATUS_CODES.OK).json("Welcome to the V-Api");
 });
 
-// Routes
 app.use("/api/auth", authRouter);
 app.use("/api/album", albumRouter);
 
-// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
-// Start server
 app.listen(Env_Consts.PORT, async () => {
     console.log(`connected at PORT : ${Env_Consts.PORT}`);
     await connectToDB();
