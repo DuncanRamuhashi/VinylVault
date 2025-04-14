e7b026074771f016158b3c07f1d9de362d36a4db
Duncan Ramuhashi <duncanramohashi@gmail.com>
2025-04-14 15:32:23 +0200

    ddd

--- vinylvault/src/Navbar.tsx
+++ vinylvault/src/Navbar.tsx
@@ -25,10 +25,13 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <div className="flex-shrink-0">
+          <a href='/'>
+          <div className="flex-shrink-0" >
             <img src={logo} alt="Company Logo" className="h-16 w-auto object-contain" />
           </div>
 
+          </a>
+ 
           {/* Burger Menu Button */}
           <button
             className="text-red-800 text-3xl lg:hidden focus:outline-none"
